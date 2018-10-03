d5494e9dc422a3d7a7e4fcbb12a83e4952674e3f
YaLong-Lee <784134748@qq.com>
2018-10-03 19:52:45 +0800

    较大幅度的更新

--- schema/Generate Xml.groovy
+++ schema/Generate Xml.groovy
@@ -13,10 +13,11 @@ import org.apache.commons.lang3.StringUtils
 
 packageName = ""
 basePackageName = ""
+idProperties = ["id"] as String[]
 gmtCreate = ["gmt_create"] as String[]
 gmtModified = ["gmt_modified"] as String[]
 isDeleteProperties = ["is_delete"] as String[]
-typeMapping = [
+javaTypeMapping = [
         (~/(?i)bigint/)                   : "Long",
         (~/(?i)int/)                      : "Integer",
         (~/(?i)float|double|decimal|real/): "Double",
@@ -24,6 +25,16 @@ typeMapping = [
         (~/(?i)date/)                     : "java.time.LocalDate",
         (~/(?i)time/)                     : "java.time.LocalTime",
         (~/(?i)/)                         : "String"
+]
+
+parameterTypeMapping = [
+        (~/(?i)bigint/)                   : "java.lang.Long",
+        (~/(?i)int/)                      : "java.lang.Integer",
+        (~/(?i)float|double|decimal|real/): "java.lang.Double",
+        (~/(?i)datetime|timestamp/)       : "java.time.LocalDateTime",
+        (~/(?i)date/)                     : "java.time.LocalDate",
+        (~/(?i)time/)                     : "java.time.LocalTime",
+        (~/(?i)/)                         : "java.lang.String"
 ]
 
 sepa = java.io.File.separator
@@ -56,19 +67,31 @@ def xml(out, tableName, className, fields) {
     out.println "<!DOCTYPE mapper PUBLIC '-//mybatis.org//DTD Mapper 3.0//EN' 'http://mybatis.org/dtd/mybatis-3-mapper.dtd' >"
     out.println "<mapper namespace='${packageName}.mapper.${className}Mapper'>"
     out.println ""
+    out.println "    <resultMap id='BaseResultMap' type='${packageName}.model.${className}Model'>"
+    out.println "        <constructor>"
+    fields.each() {
+        if (propertiesContainField(it, idProperties)) {
+            out.println "            <idArg column='${it.colName}' name = '${it.javaName}' javaType='${it.parameterType}'/>"
+        } else {
+            out.println "            <arg column='${it.colName}' name = '${it.javaName}' javaType='${it.parameterType}'/>"
+        }
+    }
+    out.println "        </constructor>"
+    out.println "    </resultMap>"
+    out.println ""
     out.println "    <sql id='Base_Column_List'>"
     out.print "    "
     fields.each() {
         if (index != 0) {
             out.print ", "
         }
-        out.print "${tableName}.${it.right}"
+        out.print "${tableName}.${it.colName}"
         index++
     }
     out.println ""
     out.println "    </sql>"
     out.println ""
-    out.println "    <select id='selectByPrimaryKey' resultType='${packageName}.model.${className}Model'"
+    out.println "    <select id='selectByPrimaryKey' resultMap='BaseResultMap'"
     out.println "            parameterType='java.lang.Long'>"
     out.println "        select "
     out.println "        <include refid='Base_Column_List'/>"
@@ -76,22 +99,22 @@ def xml(out, tableName, className, fields) {
     out.println "        where ${tableName}.id = #{id}"
     out.println "    </select>"
     fields.each() {
-        String str = it.right
+        String str = it.colName
         if (str.endsWith("_id")) {
-            def ForeignKey = javaName(it.right, true)
-            def foreignKey = javaName(it.right, false)
+            def ForeignKey = javaName(it.colName, true)
+            def foreignKey = javaName(it.colName, false)
             out.println ""
-            out.println "    <select id='selectBy${ForeignKey}' resultType='${packageName}.model.${className}Model'"
+            out.println "    <select id='selectBy${ForeignKey}' resultMap='BaseResultMap'"
             out.println "            parameterType='java.lang.Long'>"
             out.println "        select "
             out.println "        <include refid='Base_Column_List'/>"
             out.println "        from ${tableName} "
-            out.println "        where ${tableName}.${it.right} = #{${foreignKey}}"
+            out.println "        where ${tableName}.${it.colName} = #{${foreignKey}}"
             out.println "    </select>"
         }
     }
     out.println ""
-    out.println "    <select id='selectOneByQuery' resultType='${packageName}.model.${className}Model'"
+    out.println "    <select id='selectOneByQuery' resultMap='BaseResultMap'"
     out.println "            parameterType='${packageName}.model.${className}Model'>"
     out.println "        select "
     out.println "        <include refid='Base_Column_List'/>"
@@ -102,7 +125,7 @@ def xml(out, tableName, className, fields) {
     out.println "        Limit 1"
     out.println "    </select>"
     out.println ""
-    out.println "    <select id='selectByQuery' resultType='${packageName}.model.${className}Model'"
+    out.println "    <select id='selectByQuery' resultMap='BaseResultMap'"
     out.println "            parameterType='${packageName}.model.${className}Model'>"
     out.println "        select "
     out.println "        <include refid='Base_Column_List'/>"
@@ -112,50 +135,50 @@ def xml(out, tableName, className, fields) {
     out.println "        </where>"
     out.println "    </select>"
     out.println ""
-    if (propertiesContainField(isDeleteProperties, fields)) {
-        out.println "    <delete id='deleteByPrimaryKey' parameterType='java.lang.Long'>"
-        out.println "        delete from ${tableName} where ${tableName}.id = #{id}"
-        out.println "    </delete>"
-        out.println ""
-    } else {
+    if (fieldsContainProperties(isDeleteProperties, fields)) {
         out.println "    <update id='deleteByPrimaryKey' parameterType='java.lang.Long'>"
         out.println "        update ${tableName} set ${tableName}.${isDeleteProperties[0]} = 1 where ${tableName}.id = #{id}"
         out.println "    </update>"
         out.println ""
+    } else {
+        out.println "    <delete id='deleteByPrimaryKey' parameterType='java.lang.Long'>"
+        out.println "        delete from ${tableName} where ${tableName}.id = #{id}"
+        out.println "    </delete>"
+        out.println ""
     }
     fields.each() {
-        String str = it.right
+        String str = it.colName
         if (str.endsWith("_id")) {
-            def ForeignKey = javaName(it.right, true)
-            def foreignKey = javaName(it.right, false)
-            if (propertiesContainField(isDeleteProperties, fields)) {
-                out.println "    <delete id='deleteByPrimaryKey' parameterType='java.lang.Long'>"
-                out.println "        delete from ${tableName} where ${tableName}.${it.right} = #{${foreignKey}}"
-                out.println "    </delete>"
+            def ForeignKey = javaName(it.colName, true)
+            def foreignKey = javaName(it.colName, false)
+            if (fieldsContainProperties(isDeleteProperties, fields)) {
+                out.println "    <update id='deleteBy${ForeignKey}' parameterType='java.lang.Long'>"
+                out.println "        update ${tableName} set ${tableName}.${isDeleteProperties[0]} = 1 where ${tableName}.${it.colName} = #{${foreignKey}}"
+                out.println "    </update>"
                 out.println ""
             } else {
-                out.println "    <update id='deleteBy${ForeignKey}' parameterType='java.lang.Long'>"
-                out.println "        update ${tableName} set ${tableName}.${isDeleteProperties[0]} = 1 where ${tableName}.${it.right} = #{${foreignKey}}"
-                out.println "    </update>"
+                out.println "    <delete id='deleteByPrimaryKey' parameterType='java.lang.Long'>"
+                out.println "        delete from ${tableName} where ${tableName}.${it.colName} = #{${foreignKey}}"
+                out.println "    </delete>"
                 out.println ""
             }
         }
     }
-    if (propertiesContainField(isDeleteProperties, fields)) {
-        out.println "    <select id='deleteByQuery' parameterType='${packageName}.model.${className}Model'>"
-        out.println "        delete from ${tableName}"
-        out.println "        <where>"
-        out.println "            <include refid='query_filter'/>"
-        out.println "        </where>"
-        out.println "    </delete>"
-        out.println ""
-    } else {
+    if (fieldsContainProperties(isDeleteProperties, fields)) {
         out.println "    <update id='deleteByQuery' parameterType='${packageName}.model.${className}Model'>"
         out.println "        update ${tableName} set ${tableName}.${isDeleteProperties[0]} = 1"
         out.println "        <where>"
         out.println "            <include refid='query_filter'/>"
         out.println "        </where>"
         out.println "    </update>"
+        out.println ""
+    } else {
+        out.println "    <delete id='deleteByQuery' parameterType='${packageName}.model.${className}Model'>"
+        out.println "        delete from ${tableName}"
+        out.println "        <where>"
+        out.println "            <include refid='query_filter'/>"
+        out.println "        </where>"
+        out.println "    </delete>"
         out.println ""
     }
     out.println "    <select id='count' resultType='java.lang.Integer' parameterType='${packageName}.model.${className}Model'>"
@@ -165,31 +188,31 @@ def xml(out, tableName, className, fields) {
     out.println "        </where>"
     out.println "    </select>"
     out.println ""
-    out.println "    <insert id='insert' parameterType='java.util.Map' useGeneratedKeys='true' keyProperty='id'>"
+    out.println "    <insert id='insert' parameterType='${packageName}.model.${className}Model' useGeneratedKeys='true' keyProperty='id'>"
     out.println "        insert into ${tableName}"
     out.println "        <trim prefix='(' suffix=')' suffixOverrides=','>"
     fields.each() {
-        if (propertiesContainField(it.right, gmtCreate)) {
-            out.println "            ${tableName}.${it.right},"
-        } else if (propertiesContainField(it.right, gmtModified)) {
-            out.println "            ${tableName}.${it.right},"
-        } else if (propertiesContainField(it.right, isDeleteProperties)) {
-            out.println "            ${tableName}.${it.right},"
+        if (propertiesContainField(it, gmtCreate)) {
+            out.println "            ${tableName}.${it.colName},"
+        } else if (propertiesContainField(it, gmtModified)) {
+            out.println "            ${tableName}.${it.colName},"
+        } else if (propertiesContainField(it, isDeleteProperties)) {
+            out.println "            ${tableName}.${it.colName},"
         } else {
-            out.println "            <if test='${it.left} != null'>${tableName}.${it.right},</if>"
+            out.println "            <if test='${it.javaName} != null'>${tableName}.${it.colName},</if>"
         }
     }
     out.println "        </trim>"
     out.println "        <trim prefix='values (' suffix=')' suffixOverrides=','>"
     fields.each() {
-        if (propertiesContainField(it.right, gmtCreate)) {
+        if (propertiesContainField(it, gmtCreate)) {
             out.println "            now(),"
-        } else if (propertiesContainField(it.right, gmtModified)) {
+        } else if (propertiesContainField(it, gmtModified)) {
             out.println "            now(),"
-        } else if (propertiesContainField(it.right, isDeleteProperties)) {
+        } else if (propertiesContainField(it, isDeleteProperties)) {
             out.println "            0,"
         } else {
-            out.println "            <if test='${it.left} != null'>#{${it.left}},</if>"
+            out.println "            <if test='${it.javaName} != null'>#{${it.javaName}},</if>"
         }
     }
     out.println "        </trim>"
@@ -199,11 +222,11 @@ def xml(out, tableName, className, fields) {
     out.println "        update ${tableName}"
     out.println "        <set>"
     fields.each() {
-        if (propertiesContainField(it.right, gmtCreate)) {
-        } else if (propertiesContainField(it.right, gmtModified)) {
-            out.println "            ${tableName}.${it.right} = now(),"
+        if (propertiesContainField(it, gmtCreate)) {
+        } else if (propertiesContainField(it, gmtModified)) {
+            out.println "            ${tableName}.${it.colName} = now(),"
         } else {
-            out.println "            <if test='${it.left} != null'>${tableName}.${it.right} = #{${it.left}},</if>"
+            out.println "            <if test='${it.javaName} != null'>${tableName}.${it.colName} = #{${it.javaName}},</if>"
         }
     }
     out.println "        </set>"
@@ -212,10 +235,10 @@ def xml(out, tableName, className, fields) {
     out.println ""
     out.println "    <sql id='query_filter'>"
     fields.each() {
-        if (propertiesContainField(it.right, isDeleteProperties)) {
-            out.println "        and ${tableName}.${it.right} != 1"
+        if (propertiesContainField(it, isDeleteProperties)) {
+            out.println "        and ${tableName}.${it.colName} != 1"
         } else {
-            out.println "        <if test='${it.left} != null'>and ${tableName}.${it.right} = #{${it.left}}</if>"
+            out.println "        <if test='${it.javaName} != null'>and ${tableName}.${it.colName} = #{${it.javaName}}</if>"
         }
     }
     out.println "    </sql>"
@@ -223,42 +246,64 @@ def xml(out, tableName, className, fields) {
     out.println "</mapper>"
 }
 
-boolean fieldsContainPropertie(propertie, fields) {
-    def isExsit = false
-    fields.each() {
-        if (propertie == it.right) {
-            isExsit = true
+boolean fieldsContainProperties(properties, fields) {
+    def exist = false
+    properties.each() {
+        def property = it
+        fields.each() {
+            if (property == it.right) {
+                exist = true
+            }
         }
     }
-    isExsit
+    exist
 }
 
 boolean propertiesContainField(field, properties) {
-    def isExsit = false
+    def exist = false
     properties.each() {
-        if (field == it) {
-            isExsit = true
+        if (field.right == it) {
+            exist = true
         }
     }
-    isExsit
+    exist
 }
 
 def calcFields(table) {
     DasUtil.getColumns(table).reduce([]) { fields, col ->
         def spec = Case.LOWER.apply(col.getDataType().getSpecification())
-        def typeStr = typeMapping.find { p, t -> p.matcher(spec).find() }.value
+        def javaTypeStr = javaTypeMapping.find { p, t -> p.matcher(spec).find() }.value
+        def parameterTypeStr = parameterTypeMapping.find { p, t -> p.matcher(spec).find() }.value
         fields += [[
-                           left    : javaName(col.getName(), false),
-                           right   : col.getName(),
-                           name    : javaName(col.getName(), false),
-                           dataType: col.getDataType(),
-                           type    : typeStr,
-                           comment : col.getComment(),
-                           annos   : ""]]
+                           javaName     : javaName(col.getName(), false),
+                           colName      : col.getName(),
+                           parameterName: parameterName(col.getName(), true),
+                           dataType     : col.getDataType(),
+                           jdbcType     : col.getDataType(),
+                           javaType     : javaTypeStr,
+                           parameterType: parameterTypeStr,
+                           comment      : col.getComment(),
+                           annos        : ""]]
     }
 }
 
 def javaName(str, capitalize) {
+    String strTmp = str
+    if (strTmp.startsWith("is_")) {
+        str = str.replaceFirst("is_", "")
+    }
+    def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
+            .collect { Case.LOWER.apply(it).capitalize() }
+            .join("")
+            .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
+    capitalize || s.length() == 1 ? s : Case.LOWER.apply(s[0]) + s[1..-1]
+}
+
+def parameterName(str, capitalize) {
+    String strTmp = str
+    if (strTmp.startsWith("is_")) {
+        str = str.replaceFirst("is_", "")
+    }
     def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
             .collect { Case.LOWER.apply(it).capitalize() }
             .join("")
